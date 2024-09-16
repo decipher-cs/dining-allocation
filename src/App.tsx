b2c39136afc555bcacff2106a983797668266a09
foobar <uagnl25x@anonaddy.me>
2024-09-16 16:17:42 +0530

    Added confirmation dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+    const [allotmentConfirmationDialog, setAllotmentConfirmationDialog] = useState(true)
+    const [allotmentStatus, setAllotmentStatus] = useState<'loading' | 'success' | 'failed' | 'uninitiated'>(
+        'uninitiated'
+    )
+
+    const reserveSeat = (seatNum: number) => {
+        setAllotmentConfirmationDialog(true)
+        setAllotmentStatus('loading')
+    }
+
+    useEffect(() => {
+        let id = undefined
+        if (allotmentStatus === 'loading') {
+            id = setTimeout(() => {
+                setAllotmentStatus('success')
+            }, 1000 * 3)
+        }
+        return () => {
+            clearTimeout(id)
+        }
+    }, [allotmentStatus])
+
     return (
         <main>
             <form
@@ -46,14 +70,49 @@ function App() {
                 {Array(20)
                     .fill(' ')
                     .map((_, tableNum) => {
-                        const seatAvailable = Boolean(Math.floor(Math.random() * 1.3))
+                        const seatAvailable = Boolean(Math.floor(Math.random() * 2))
                         return (
-                            <div key={tableNum} className={[seatAvailable ? 'bg-green-400' : 'bg-red-400'].join(' ')}>
+                            <button
+                                disabled={!seatAvailable}
+                                key={tableNum}
+                                className={[
+                                    seatAvailable ? 'bg-green-400 cursor-pointer' : 'bg-red-400 cursor-not-allowed',
+                                ].join(' ')}
+                                type="button"
+                                onClick={() => {
+                                    reserveSeat(tableNum)
+                                }}
+                            >
                                 {tableNum}
-                            </div>
+                            </button>
                         )
                     })}
             </section>
+
+            {allotmentConfirmationDialog && (
+                <dialog
+                    open={allotmentConfirmationDialog}
+                    className="h-svh w-full absolute inset-0 grid place-content-center"
+                    style={{
+                        backdropFilter: 'blur(4px) saturate(80%)',
+                        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                    }}
+                >
+                    <button
+                        className="p-2 bg-black text-white"
+                        onClick={() => setAllotmentConfirmationDialog(false)}
+                        aria-label="close dialog"
+                    >
+                        Close X
+                    </button>
+
+                    {allotmentStatus === 'loading' && <div className="bg-white w-fit p-4">Loading...</div>}
+                    {allotmentStatus === 'failed' && (
+                        <div className="bg-white w-fit p-4">Sorry try some other table</div>
+                    )}
+                    {allotmentStatus === 'success' && <div className="bg-white w-fit p-4">Successful ✅</div>}
+                </dialog>
+            )}
         </main>
     )
 }
