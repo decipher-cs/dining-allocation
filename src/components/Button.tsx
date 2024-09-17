8625cc02d024256d8252a73c677dc7155cd7f87b
foobar <uagnl25x@anonaddy.me>
2024-09-17 16:43:32 +0530

    used focus-visible for better accessibility and design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,8 @@ export default function Button({ variant = "primary", ...props }: ButtonProps) {
         <button
             className={twMerge(
                 `border-primary bg-primary [&:not(:disabled)]:hover:text-primary active:text-primary inline-block rounded-lg border
-                text-sm font-medium text-white transition focus:outline-none focus:ring [&:not(:disabled)]:hover:bg-transparent`,
+                text-sm font-medium text-white transition focus:outline-none focus-visible:outline-white
+                [&:not(:disabled)]:hover:bg-transparent`,
                 className
             )}
             {...rest}
