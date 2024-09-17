3404341203e6b763db510f6302590621b0cb5cae
foobar <uagnl25x@anonaddy.me>
2024-09-17 14:24:16 +0530

    added animation for loading

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         }
     }, [seatDate])
 
-    const [allotmentConfirmationDialog, setAllotmentConfirmationDialog] = useState(true)
+    const [allotmentConfirmationDialog, setAllotmentConfirmationDialog] = useState(false)
 
     const [allotmentStatus, setAllotmentStatus] = useState<"loading" | "success" | "failed" | "uninitiated">("success")
 
@@ -168,15 +168,34 @@ function App() {
                         backgroundColor: "rgba(0, 0, 0, 0.1)",
                     }}
                 >
-                    <div className="rounded-xl border border-gray-100 bg-white p-4">
-                        {allotmentStatus === "loading" && <div className="w-fit bg-white p-4">Loading...</div>}
+                    <div className="rounded-xl border border-gray-100 bg-white p-7">
+                        {allotmentStatus === "loading" && (
+                            <div className="w-fit animate-spin bg-white p-4">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                                    />
+                                </svg>
+                                <span className="sr-only">Loading. Please Wait</span>
+                            </div>
+                        )}
                         {allotmentStatus === "failed" && (
                             <div className="w-fit bg-white p-4">Sorry try some other table</div>
                         )}
                         {allotmentStatus === "success" && (
-                            <div className="flex items-start gap-4">
-                                <div className="flex-1">
+                            <div className="flex items-start gap-5">
+                                <div className="flex-1 space-y-3">
                                     <strong className="flex gap-2 font-medium text-gray-900">
+                                        Successful
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
@@ -191,7 +210,6 @@ function App() {
                                                 d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                                             />
                                         </svg>
-                                        Successful
                                     </strong>
 
                                     <p className="mt-1 text-sm text-gray-700">
@@ -208,7 +226,7 @@ function App() {
                                     </Button>
                                 </div>
 
-                                <Button className="bg-transparen border-none px-3 text-gray-500 transition hover:text-gray-600">
+                                <Button className="border-none bg-transparent px-3 text-gray-500 transition hover:text-gray-600">
                                     <span className="sr-only">close dialog</span>
 
                                     <svg
